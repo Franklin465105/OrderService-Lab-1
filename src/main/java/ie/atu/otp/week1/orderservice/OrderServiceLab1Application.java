c8fb19ed35088f7d4bdec4b0c585ce72d6100665
Franklin Esedebe <G00465105@atu.ie>
2026-09-16 15:21:34 +0100

    Set up 2nd microservice

---
+++ src/main/java/ie/atu/otp/week1/orderservice/OrderServiceLab1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.otp.week1.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceLab1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/otp/week1/orderservice/OrderServiceLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.otp.week1.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
