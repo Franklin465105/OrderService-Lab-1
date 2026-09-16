34c9bb00be1fdb4b3e384bc133cae7bfccb80b31
Franklin Esedebe <G00465105@atu.ie>
2026-09-16 15:36:12 +0100

    Added PurchaseorderController in the controller package

--- src/main/java/ie/atu/otp/week1/orderservice/controller/PurchaseOrderController.java
+++ src/main/java/ie/atu/otp/week1/orderservice/controller/PurchaseOrderController.java
@@ -1,4 +1,25 @@
 package ie.atu.otp.week1.orderservice.controller;
 
+import ie.atu.otp.week1.orderservice.model.PurchaseOrder;
+import ie.atu.otp.week1.orderservice.service.PurchaseOrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/orders")
 public class PurchaseOrderController {
+    private final PurchaseOrderService service;
+    public PurchaseOrderController(PurchaseOrderService service) {
+        this.service = service;
+    }
+    @GetMapping
+    public List<PurchaseOrder> getAll() {
+        return service.getAll();
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PurchaseOrder create(@RequestBody PurchaseOrder order) {
+        return service.create(order);
+    }
 }
+
