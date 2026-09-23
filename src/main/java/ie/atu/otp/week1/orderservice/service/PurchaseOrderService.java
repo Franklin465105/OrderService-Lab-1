c1883cb6c1ef3d4269ac750f497415969ec6b057
Franklin Esedebe <G00465105@atu.ie>
2026-09-23 14:48:47 +0100

    Refactored PurchaseOrderService, with Repository which springboot injects in

--- src/main/java/ie/atu/otp/week1/orderservice/service/PurchaseOrderService.java
+++ src/main/java/ie/atu/otp/week1/orderservice/service/PurchaseOrderService.java
@@ -1,19 +1,23 @@
 package ie.atu.otp.week1.orderservice.service;
 
 import ie.atu.otp.week1.orderservice.model.PurchaseOrder;
+import ie.atu.otp.week1.orderservice.repository.PurchaseOrderRepository;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
 @Service
 public class PurchaseOrderService {
-    private final List<PurchaseOrder> orders = new ArrayList<>();
-    private long nextId = 1;
-    public List<PurchaseOrder> getAll() {
-        return orders;
+    private final PurchaseOrderRepository repository;
+    public PurchaseOrderService(PurchaseOrderRepository repository)
+    {
+        this.repository = repository;
     }
-    public PurchaseOrder create(PurchaseOrder order) {
-        order.setId(nextId++);
-        orders.add(order);
-        return order;
+    public List<PurchaseOrder> getAll()
+    {
+        return repository.findAll();
+    }
+    public PurchaseOrder create(PurchaseOrder order)
+    {
+        order.setId(null);
+        return repository.save(order);
     }
 }
